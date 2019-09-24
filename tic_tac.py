949ce94c4ab0ff25cb079639acab65208ab2ada8
Raven Robertson <ravewillow6383@gmail.com>
2019-09-23 17:45:12 -0700

    basic files added

---
+++ tic_tac.py
@@ -0,0 +1,18 @@
+# Board to display
+# Play a game
+# Handle a turn
+# function to check win
+    # check rows
+    # check columns
+    # check diagonals
+# function to check for tie
+# function to change players
+
+# Global constants:
+
+columns = 3
+rows = 3
+
+setting up a game board
+board = [['-' for n in range(columns)] for n in range (rows)]
+

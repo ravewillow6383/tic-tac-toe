e34a6384a3b0de9da4e07682c988d7338b448fbc
Raven Robertson <ravewillow6383@gmail.com>
2019-09-23 18:42:51 -0700

    input correctly places x mark on given numbered slot

--- tic_tac.py
+++ tic_tac.py
@@ -13,6 +13,67 @@
 columns = 3
 rows = 3
 
-setting up a game board
+# setting up a game board
 board = [['-' for n in range(columns)] for n in range (rows)]
 
+def display_board():
+    for row in board:
+        print(row)
+
+def handle_turn():
+    try:
+        position =  """
+        **************************************
+        **    Try your luck at tic tac toe! **
+        **   The game board reads left to   **
+        **        right, top to bottom      **
+        **   and are in numerical order.    **
+        **************************************
+        #####################################################
+
+        Choose a whole number between 1 and 9, please: 
+    
+        #####################################################
+        """ 
+        position = input(position)
+        position = int(position) - 1
+        if position >= 0 and position <= 2:
+            board[0][position] = 'X'
+        elif position >= 3 and position <= 5:
+            position -= 3
+            board[1][position] = 'X'
+        elif position > 5 and position <= 8:
+            position -= 6
+            board[2][position] = 'X'
+        elif position < 0 or position > 8:
+            raise ValueError('please enter a whole number between 1-9')
+        display_board()
+    except ValueError:
+        print('Ahem. Pick yourself up, dust yourself off and try that one again.') 
+        handle_turn() 
+
+def check_if_game_over():
+    check_for_win()
+    check_for_tie()      
+
+def play_game():
+    try:
+
+        while game_is_still_going:
+
+            handle_turn(current_player)
+
+            check_if_game_over()
+
+            flip_player()
+    
+    except KeyboardInterrupt:
+        print('Don\'t quit on me now!')
+
+    # Display initial board
+    # display_board()
+
+
+if __name__ == '__main__':
+
+    play_game()
